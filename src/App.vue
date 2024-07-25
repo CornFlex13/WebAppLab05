4ac2e78cacea4254d17cfa023cdd097cf15d34fa
CornFlex13 <pammer434@gmail.com>
2024-07-25 23:02:44 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { onMounted } from 'vue';
+import { useTableStore } from './store/table';
+import { useQueueStore } from './store/queue';
+const tableStore = useTableStore();
+onMounted(async () => {
+  await tableStore.fetchTables();
+});
+const queueStore = useQueueStore();
+onMounted(async () => {
+  await queueStore.fetchQueues();
+});
+</script>
+
+<template>
+  <VApp>
+    <RouterView />
+  </VApp>
+</template>
